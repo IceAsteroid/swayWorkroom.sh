93b49aca3833604593fc31e05afca7e0b2fc2853
IceAsteroid <90975914+IceAsteroid@users.noreply.github.com>
2023-03-08 19:34:52 +0000

    Add files via upload

---
+++ swayWorkroom.sh
@@ -0,0 +1,72 @@
+#!/bin/bash
+
+swayScriptsTmpDir="/tmp/swayScriptsTmpDir"
+[ -d "${swayScriptsTmpDir}" ] || { mkdir ${swayScriptsTmpDir}; chmod 770 ${swayScriptsTmpDir}; }
+
+workroomSwitchedFile="${swayScriptsTmpDir}/swayWorkroomSwitched.txt"
+[ -f "${workroomSwitchedFile}" ] || { touch ${workroomSwitchedFile}; chmod 660 ${workroomSwitchedFile}; }
+# Initiate to workrrom 1 if no previous one is stored in file.
+[ -s "${workroomSwitchedFile}" ] || echo "1" > ${workroomSwitchedFile}
+
+workroomWorkspaceSwitchedFile="${swayScriptsTmpDir}/swayWorkroomWorkspaceSwitched.txt"
+[ -f "${workroomWorkspaceSwitchedFile}" ] || { touch ${workroomWorkspaceSwitchedFile}; chmod 660 ${workroomWorkspaceSwitchedFile}; }
+
+workspaceToSwitch="$(cat ${workroomWorkspaceSwitchedFile})"
+
+case ${1} in
+  -q) echo "1" > ${workroomSwitchedFile}
+      workroomToSwitch="$(cat ${workroomSwitchedFile})"
+      swaymsg "workspace ${workroomToSwitch}${workspaceToSwitch}";;
+  -w) echo "2" > ${workroomSwitchedFile}
+      workroomToSwitch="$(cat ${workroomSwitchedFile})"
+      swaymsg "workspace ${workroomToSwitch}${workspaceToSwitch}";;
+  -e) echo "3" > ${workroomSwitchedFile}
+      workroomToSwitch="$(cat ${workroomSwitchedFile})"
+      swaymsg "workspace ${workroomToSwitch}${workspaceToSwitch}";;
+  -x) workroomToSwitch="$(cat ${workroomSwitchedFile})";;
+esac
+
+if [[ -v 2 && ! "${2}" = 0 ]]; then
+  case ${2} in
+    1) swaymsg "workspace ${workroomToSwitch}1";
+       echo "1" > ${workroomWorkspaceSwitchedFile};;
+    2) swaymsg "workspace ${workroomToSwitch}2";
+       echo "2" > ${workroomWorkspaceSwitchedFile};;
+    3) swaymsg "workspace ${workroomToSwitch}3";
+       echo "3" > ${workroomWorkspaceSwitchedFile};;
+    4) swaymsg "workspace ${workroomToSwitch}4";
+       echo "4" > ${workroomWorkspaceSwitchedFile};;
+    5) swaymsg "workspace ${workroomToSwitch}5";
+       echo "5" > ${workroomWorkspaceSwitchedFile};;
+    6) swaymsg "workspace ${workroomToSwitch}6";
+       echo "6" > ${workroomWorkspaceSwitchedFile};;
+    7) swaymsg "workspace ${workroomToSwitch}7";
+       echo "7" > ${workroomWorkspaceSwitchedFile};;
+    8) swaymsg "workspace ${workroomToSwitch}8";
+       echo "8" > ${workroomWorkspaceSwitchedFile};;
+    9) swaymsg "workspace ${workroomToSwitch}9";
+       echo "9" > ${workroomWorkspaceSwitchedFile};;
+    --to1) swaymsg "move container to workspace number ${workroomToSwitch}1";;
+    --to2) swaymsg "move container to workspace number ${workroomToSwitch}2";;
+    --to3) swaymsg "move container to workspace number ${workroomToSwitch}3";;
+    --to4) swaymsg "move container to workspace number ${workroomToSwitch}4";;
+    --to5) swaymsg "move container to workspace number ${workroomToSwitch}5";;
+    --to6) swaymsg "move container to workspace number ${workroomToSwitch}6";;
+    --to7) swaymsg "move container to workspace number ${workroomToSwitch}7";;
+    --to8) swaymsg "move container to workspace number ${workroomToSwitch}8";;
+    --to9) swaymsg "move container to workspace number ${workroomToSwitch}9";;
+    --to0) swaymsg "move container to workspace number $((${workroomToSwitch} + 1))0";;
+  esac
+else
+  #workspaceToSwitch="$(cat ${workroomWorkspaceSwitchedFile})"
+  if [ "${2}" = 0 ]; then
+    swaymsg "workspace $((${workroomToSwitch} + 1))0"; echo "0" > ${workroomWorkspaceSwitchedFile}
+  else
+    if [ "${workspaceToSwitch}" = 0 ]; then
+      swaymsg "workspace $((${workroomToSwitch} + 1))0"; echo "0" > ${workroomWorkspaceSwitchedFile}
+    else
+      swaymsg "workspace ${workroomToSwitch}${workspaceToSwitch}"
+    fi
+  fi
+fi
+    
